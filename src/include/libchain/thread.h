aa3a0256c633a1dd4dda58cb8a451e9b6ee74f32
soctar <soctar@gmail.com>
2017-03-05 21:40:50 -0500

    Changed Makefile to include libchain object Added doxygen comments for thread.h Lots of fixes to thread.c so it would compile with chain - mostly silly mistakes that took a bunch of reading through complex compilier warnings

--- src/include/libchain/thread.h
+++ src/include/libchain/thread.h
@@ -1,31 +1,37 @@
-/** @file scheduler.h
- *  @brief TODO
- *
+/** @file thread.h
+ *  @brief Interface for multi-threaded chain functions
  *
  */
 
-#ifndef _SCHEDULER_H
-#define _SCHEDULER_H
+#ifndef _THREAD_H
+#define _THREAD_H
 
-#include <chain.h>
+#include "chain.h"
 
 typedef struct thread_t {
     // TODO - overflow is possible!
     unsigned thread_id;
     context_t context;
-}
+} thread_t;
 
-/** @brief Initialize scheduler */
+/** @brief Initialize scheduler constructs at first boot */
 void thread_init();
 
-/** @brief TODO
+/** @brief Terminate execution of a thread, removing it from
+ *         the scheduling pool
+ *  @return Void
  */
 void thread_end();
 
-/** @brief TODO */
+/** @brief Creates a separate thread to run task new_task
+ *  @param new_task Task entry point for the thread
+ *  @return 0 on success, a negative error code on failure
+ */
 int thread_create(task_t *new_task);
 
-/** @brief TODO */
-thread_t * volatile get_current_thread();
+/** @brief Gets the currently running thread
+ *  @return Pointer to the struct describing the current thread
+ */
+thread_t *get_current_thread();
 
 #endif
--- src/thread.c
+++ src/thread.c
@@ -1,14 +1,11 @@
 /** @file thread.c
- *  @brief TODO
- *
+ *  @brief Implementation of multi-threading functions
  */
 
 #include "chain.h"
-#include <thread.h>
+#include "thread.h"
 
 #define MAX_NUM_THREADS 4
-#define INACTIVE 0
-#define ACTIVE 1
 
 typedef struct thread_state_t {
     thread_t thread;
@@ -16,24 +13,34 @@ typedef struct thread_state_t {
 } thread_state_t;
 
 struct thread_array {
-    thread_state_t threads[MAX_NUM_THREADS];
-    unsigned current;
-    unsigned num_threads;
+    SELF_CHAN_FIELD_ARRAY(thread_state_t, threads, MAX_NUM_THREADS);
+    SELF_CHAN_FIELD(unsigned, current);
+    SELF_CHAN_FIELD(unsigned, num_threads);
 };
 
 #define FIELD_INIT_thread_array {\
-    SELF_FIELD_ARRAY_INITIALIZER(NUM_THREADS),\
+    SELF_FIELD_ARRAY_INITIALIZER(MAX_NUM_THREADS),\
     SELF_FIELD_INITIALIZER,\
     SELF_FIELD_INITIALIZER\
 }
+
+TASK(1, scheduler_task)
+
+// Empty task so we can create a self-channel
+void scheduler_task() {
+    return;
+}
+
 
 SELF_CHANNEL(scheduler_task, thread_array);
 
 void thread_init() {
     thread_state_t *threads = *CHAN_IN1(thread_state_t *, threads,
             SELF_IN_CH(scheduler_task));
-    for (unsigned i = 0; i < MAX_NUM_THREADS; i++) {
-        threads[i].active = INACTIVE;
+    threads[0].active = 1;
+
+    for (unsigned i = 1; i < MAX_NUM_THREADS; i++) {
+        threads[i].active = 0;
     }
 }
 
@@ -41,8 +48,9 @@ void thread_end() {
     thread_state_t *threads = *CHAN_IN1(thread_state_t *, threads,
             SELF_IN_CH(scheduler_task));
     unsigned current = *CHAN_IN1(unsigned, current, SELF_CH(scheduler_task));
-    threads[current].active = INACTIVE;
-    CHAN_OUT1(thread_state_t, threads[current], SELF_OUT_CH(scheduler_task));
+    threads[current].active = 0;
+    CHAN_OUT1(thread_state_t, threads[current], threads[current],
+            SELF_OUT_CH(scheduler_task));
 }
 
 int thread_create(task_t *new_task) {
@@ -52,24 +60,26 @@ int thread_create(task_t *new_task) {
             SELF_CH(scheduler_task));
     if (num_threads == MAX_NUM_THREADS) { return -1;}
     for (unsigned i = 0; i < MAX_NUM_THREADS; i++) {
-        if (threads[i].active = INACTIVE) {
+        if (!threads[i].active) {
             threads[i].thread.context.task = new_task;
             // TODO Set to creation time instead of 0?
             threads[i].thread.context.time = 0;
             threads[i].thread.context.next_ctx = NULL;
             num_threads++;
-            CHAN_OUT1(thread_state_t, threads[i], SELF_OUT_CH(scheduler_task));
-            CHAN_OUT1(unsigned, num_threads, SELF_OUT_CH(scheduler_task));
+            CHAN_OUT1(thread_state_t, threads[i], threads[i],
+                    SELF_OUT_CH(scheduler_task));
+            CHAN_OUT1(unsigned, num_threads, num_threads,
+                    SELF_OUT_CH(scheduler_task));
             return 0;
         }
     }
     return -2;
 }
 
-thread_t * volatile get_current_thread() {
+thread_t *get_current_thread() {
     thread_state_t *threads = *CHAN_IN1(thread_state_t *, threads,
             SELF_IN_CH(scheduler_task));
     unsigned current = *CHAN_IN1(unsigned, current,
             SELF_CH(scheduler_task));
-    return threads[current];
+    return &threads[current].thread;
 }
